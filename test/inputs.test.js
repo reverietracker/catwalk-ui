f4165bec3a93d0fa5bbb2415970137a05336a863
Matt Westcott <matt@west.co.tt>
2023-11-24 01:03:06 +0000

    add tests

---
+++ test/inputs.test.js
@@ -0,0 +1,42 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { Model, fields } = require('catwalk');
+const { NumberInput } = require('../');
+
+class Rectangle extends Model([
+    new fields.IntegerField('width', {min: 1, max: 100}),
+    new fields.IntegerField('height', {min: 1, max: 100}),
+]) {
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+test('NumberInput sets min/max on input', () => {
+    const widthInput = new NumberInput({property: 'width', changeEvent: 'changeWidth', min: 1, max: 100});
+    document.body.appendChild(widthInput.node);
+    expect(widthInput.node.min).toBe("1");
+    expect(widthInput.node.max).toBe("100");
+});
+
+test("writing to NumberInput doesn't break when not following a model", () => {
+    const widthInput = new NumberInput({property: 'width', changeEvent: 'changeWidth'});
+    document.body.appendChild(widthInput.node);
+    widthInput.node.value = "60";
+    widthInput.node.dispatchEvent(new Event('change'));
+    expect(widthInput.node.value).toBe("60");
+});
+
+test('NumberInput can follow model', () => {
+    const rect = new Rectangle({width: 10, height: 20});
+    const widthInput = new NumberInput({property: 'width', changeEvent: 'changeWidth'});
+    document.body.appendChild(widthInput.node);
+    widthInput.followModel(rect);
+    rect.width = 50;
+    expect(widthInput.node.value).toBe("50");
+    widthInput.node.value = "60";
+    widthInput.node.dispatchEvent(new Event('change'));
+    expect(rect.width).toBe(60);
+});
