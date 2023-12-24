38a6c5f5006aad0307dbe8db4af267a6a54f6dc3
Matt Westcott <matt@west.co.tt>
2023-12-24 00:53:53 +0000

    handle class/className attribute in JSX runtime

--- jsx-runtime.js
+++ jsx-runtime.js
@@ -25,6 +25,12 @@ function jsxs(name, attrs) {
             for (const [styleName, styleValue] of Object.entries(value)) {
                 elem.style[styleName] = styleValue;
             }
+        } else if (attr == 'className') {
+            if (Array.isArray(value)) {
+                elem.classList.add(...value);
+            } else {
+                elem.classList.add(value);
+            }
         } else {
             elem.setAttribute(attr, value);
         }
